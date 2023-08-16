6193ac0e09d92b193cc4e5e8f31b134baac304a9
matszpk <matszpk@interia.pl>
2023-08-16 16:20:56 +0200

    Add testcase for signed divmod.

--- src/boolexpr_creator.rs
+++ src/boolexpr_creator.rs
@@ -1086,5 +1086,48 @@ mod tests {
                 }
             }
         }
+        
+        let ec = ExprCreator::<isize>::new();
+        let a = IntExprNode::<_, U8, true>::variable(ec.clone());
+        let b = IntExprNode::<_, U8, true>::variable(ec.clone());
+        let (d, m, c) = a.clone().divmod(b.clone());
+        let mut indexes = [0; 17];
+        (0..8).for_each(|x| indexes[x] = d.bit(x).index);
+        (0..8).for_each(|x| indexes[8 + x] = m.bit(x).index);
+        indexes[16] = c.index;
+        let (circuit, input_map) = ec.borrow().to_circuit(indexes);
+        for av in i8::MIN..=i8::MAX {
+            for bv in i8::MIN..=i8::MAX {
+                let exp_cv = (bv != 0) && (av != i8::MIN || bv != -1);
+                let exp_dv = if exp_cv { av / bv } else { 0 };
+                let exp_mv = if exp_cv { av % bv } else { 0 };
+                let mut input = [false; 16];
+                (0..8).for_each(|x| {
+                    input[input_map[&a.bit(x).varlit().unwrap()]] = ((av >> x) & 1) != 0;
+                    input[input_map[&b.bit(x).varlit().unwrap()]] = ((bv >> x) & 1) != 0;
+                });
+                let rv_vec = circuit.eval(input);
+                let mut dv = 0;
+                let mut mv = 0;
+                let mut cv = false;
+                rv_vec.into_iter().enumerate().for_each(|(i, bv)| {
+                    if bv {
+                        if i < 8 {
+                            dv |= 1 << i;
+                        } else if i < 16 {
+                            mv |= 1 << (i - 8);
+                        } else {
+                            cv = true;
+                        }
+                    }
+                });
+                println!("dcfv: {} {}: {} {} {}", av, bv, dv, mv, cv);
+                assert_eq!(exp_cv, cv, "divmod({}, {}).c", av, bv);
+                if exp_cv {
+                    assert_eq!(exp_dv, dv, "divmod({}, {}).d", av, bv);
+                    assert_eq!(exp_mv, mv, "divmod({}, {}).m", av, bv);
+                }
+            }
+        }
     }
 }
